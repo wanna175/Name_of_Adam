7c3264e219579770d81e004c3c9b5efd591432f7
ch-0012 <73748422+ch-0012@users.noreply.github.com>
2023-02-02 23:21:17 +0900

    refactor : Stat 위치 수정 (Define)

--- Assets/Script/Utils/Define.cs
+++ Assets/Script/Utils/Define.cs
@@ -1,13 +1,23 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
+[Serializable]
+public struct Stat
+{
+    public float HP;
+    public float ATK;
+    public int SPD;
+}
 
+[Serializable]
 public enum RangeType
 {
     Melee,
     Ranged
 }
 
+[Serializable]
 public enum Team
 {
     Player,
