e89fbf58cd0e426fe696f1ca3099c6be7430e18b
dylk92 <36599040+dylk92@users.noreply.github.com>
2023-02-11 19:17:49 +0900

    fix : ClickState

--- Assets/Script/Manager/BattleManager/BattleManager.cs
+++ Assets/Script/Manager/BattleManager/BattleManager.cs
@@ -22,18 +22,6 @@ public class BattleManager : MonoBehaviour
 
     [SerializeField] private bool TestMode = true;
     private ClickType _clickType;
-    public void ChangeClickType()
-    {
-        _clickType++;
-
-        if(_clickType > ClickType.Attack)
-        {
-            _clickType = ClickType.Nothing;
-        }
-
-        SetTileColor(Color.yellow);
-        Debug.Log(_clickType);
-    }
 
     private void Awake()
     {
@@ -132,58 +120,7 @@ public class BattleManager : MonoBehaviour
                 GetNowUnit().MoveTileClick(coord);
             else if (_clickType == ClickType.Attack)
                 GetNowUnit().AttackTileClick(coord);
-
-            //GetNowUnit().TileSelected(coord);
-
-            // 코루틴 체크
-            return;
         }
-    }
-
-    public IEnumerator EngageClickCheck()
-    {
-        while (true)
-        {
-            Debug.Log("while 진입");
-            yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
-
-            Debug.Log("Input 진입");
-            RaycastHit2D[] hits = GameManager.CutScene.CameraHandler.CameraRayCast();
-            Vector2 coord = new Vector2(-1, -1);
-            Debug.Log(hits.Length);
-
-            foreach (RaycastHit2D hit in hits)
-            {
-                Tile tile = hit.collider.transform.GetComponent<Tile>();
-                Debug.Log(tile);
-                if (tile != null)
-                {
-                    Debug.Log("Tile은 존재함");
-                    if (_field.TileDict.ContainsValue(tile))
-                    {
-                        coord = _field.FindCoordByTile(tile);
-                        Debug.Log("Tile의 좌표는 " + coord);
-                        break;
-                    }
-                    Debug.Log("Tile을 필드에서 찾지 못함");
-                }
-                else
-                {
-                    Debug.Log("타일이 클릭되지 않음");
-                }
-            }
-
-
-            if (coord != new Vector2(-1, -1))
-            {
-                Debug.Log("Tile을 찾고 좌표를 확인함");
-                _field.ClearAllColor();
-                //GetNowUnit().TileSelected(coord);
-                break;
-            }
-        }
-
-        Debug.Log("while 종료");
     }
     
     #region Phase Control
@@ -332,6 +269,22 @@ public class BattleManager : MonoBehaviour
         _BattleUnitOrderList.Remove(_unit);
     }
 
+    public void ChangeClickType()
+    {
+        if (GetNowUnit().Team == Team.Enemy)
+            return;
+
+        _clickType++;
+
+        if (_clickType > ClickType.Attack)
+        {
+            _clickType = ClickType.Nothing;
+        }
+
+        SetTileColor(Color.yellow);
+        Debug.Log(_clickType);
+    }
+
     // BattleUnitList의 첫 번째 요소부터 순회
     // 다음 차례의 공격 호출은 CutSceneMNG의 ZoomOut에서 한다.
     public void UseUnitSkill()
@@ -352,8 +305,7 @@ public class BattleManager : MonoBehaviour
             }
             else
             {
-                //StartCoroutine(EngageClickCheck());
-                _clickType = ClickType.Move;
+                ChangeClickType();
             }
         }
         else
@@ -391,157 +343,5 @@ public class BattleManager : MonoBehaviour
     }
 
     public BattleUnit GetNowUnit() => _BattleUnitOrderList[0];
-
-    #region AI
-    public void BattleUnitAI()
-    {
-        List<Vector2> FindTileList = new List<Vector2>();
-        List<Vector2> RangedVectorList = new List<Vector2>();
-
-        List<Vector2> AttackRangeList = _BattleUnitOrderList[0].GetAttackRange();
-
-        //전달받은 범위에서 유닛을 찾는다.
-        foreach (Vector2 arl in AttackRangeList)
-        {
-            Vector2 vector = _BattleUnitOrderList[0].Location;
-
-            if (Field.IsInRange(vector))
-            {
-                Vector2 vec = vector;
-                if (_field.TileDict[vec].IsOnTile)
-                {
-                    FindTileList.Add(vec);
-                }
-            }
-        }
-
-        //찾은 유닛이 있는지 확인하고, 있다면 원거리인지, 근거리인지 확인한다.
-        if (FindTileList.Count > 0)
-        {
-            foreach (Vector2 ftl in FindTileList)
-            {
-                if (_field.TileDict[ftl].Unit.Data.BehaviorType == BehaviorType.원거리)
-                {
-                    RangedVectorList.Add(ftl);
-                }
-            }
-
-            if (RangedVectorList.Count > 0)
-            {
-                //원거리 유닛이 있을 경우
-                //Random.Range(0, RangeList.Count);
-            }
-            else
-            {
-                //근거리 유닛만 있을 경우
-                //Random.Range(0, findUnitList.Count);
-            }
-        }
-        else
-        {
-            //공격 범위 내에서 찾은 유닛이 없으면 이동하고 공격한다
-            SortedSet<Vector3> AttackTileSet = new SortedSet<Vector3>();
-
-            //모든 공격 타일을 AttackTileSet에 저장한다. X, Y는 좌표, Z는 원거리/근거리 유무
-            foreach(BattleUnit unit in _battleData.BattleUnitList)
-            {
-                if (unit.Team == Team.Player)
-                {
-                    foreach (Vector2 arl in AttackRangeList)
-                    {
-                        Vector3 vector = unit.Location - arl;
-                        if (unit.Data.BehaviorType == BehaviorType.원거리)
-                            vector.z = 0f;//원거리면 0
-                        else
-                            vector.z = 0.1f;//근거리면 0.1
-
-
-                        AttackTileSet.Add(vector);
-                    }
-                }
-            }
-
-            //유닛을 때릴 수 있는 타일이 이동 범위 내에 있는 지 확인한다.
-            //단 위, 아래, 왼, 오른쪽만 이동 가능하다고 가정
-            for (int i = -1; i <= 1; i += 2)
-            {
-                for (float j = 0; j <= 0.1f; j += 0.1f)
-                {
-                    Vector3 vec1 = new Vector3(_BattleUnitOrderList[0].Location.x + i, _BattleUnitOrderList[0].Location.y, j);
-                    if (AttackTileSet.Contains(vec1))
-                    {
-                        FindTileList.Add(vec1);
-                    }
-
-                    Vector3 vec2 = new Vector3(_BattleUnitOrderList[0].Location.x, _BattleUnitOrderList[0].Location.y + i, j);
-                    if (AttackTileSet.Contains(vec2))
-                    {
-                        FindTileList.Add(vec2);
-                    }
-                }
-            }
-
-
-            if (FindTileList.Count > 0)
-            {
-                //이동해서 갈 수 있는 공격 타일이 있을 경우
-                foreach(Vector3 v in FindTileList)
-                {
-                    if (v.z == 0)
-                    {
-                        RangedVectorList.Add(new Vector2(v.x, v.y));
-                    }
-                }
-
-                if (RangedVectorList.Count > 0)
-                {
-                    //원거리가 있음
-                    //Random.Range(0, RangedVectorList.Count);
-                }
-                else
-                {
-                    //근거리만 있음
-                    //Random.Range(0, FindTileList.Count);
-                }
-            }
-            else
-            {
-                Vector3 MyPosition = _BattleUnitOrderList[0].Location;
-
-                float dis = 100f;
-                Vector3 minVec = new Vector3();
-
-                foreach (Vector3 v in RangedVectorList)
-                {
-                    if (dis > Mathf.Abs(v.x - MyPosition.x) + Mathf.Abs(v.y - MyPosition.y)) 
-                    {
-                        dis = Mathf.Abs(v.x - MyPosition.x) + Mathf.Abs(v.y - MyPosition.y);
-                        minVec = v;
-                    }
-                }
-                //가장 가까운 타일 = minVec으로 이동
-
-                dis = 100f;//재활용
-                Vector3 moveVec = new Vector3();
-                for (int i = -1; i <= 1; i += 2)
-                {
-                    Vector3 vec1 = new Vector3(MyPosition.x + i, MyPosition.y, 0);
-                    if (dis > (vec1 - minVec).sqrMagnitude) 
-                    {
-                        dis = (vec1 - minVec).sqrMagnitude;
-                        moveVec = vec1;
-                    }
-
-                    Vector3 vec2 = new Vector3(MyPosition.x, MyPosition.y + i, 0);
-                    if (dis > (vec2 - minVec).sqrMagnitude)
-                    {
-                        dis = (vec2 - minVec).sqrMagnitude;
-                        moveVec = vec2;
-                    }
-                }
-                //moveVec으로 이동
-            }
-        }
-    }
-    #endregion
+    
 }
