bed165402b103991820b2544ab6d5fdc7ad81d21
dylk92 <dylk92@naver.com>
2023-03-18 03:28:07 +0900

    EventScene 구성

---
+++ Assets/Script/EventScene/EventButtonEventTrigger.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventButtonEventTrigger : EventTrigger
+{
+    Action<GameObject> PointerClick;
+
+    public void Init(EventController controller)
+    {
+        PointerClick += controller.ButtonClick;
+    }
+
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        PointerClick(gameObject);
+    }
+}
