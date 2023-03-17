633fd7c018b92661494acda0d08420ba6d3bb342
ch-0012 <73748422+ch-0012@users.noreply.github.com>
2023-03-17 20:13:04 +0900

    fix : 턴 체인지 버튼 눌러야 턴 넘어가도록 수정

--- Assets/Script/Manager/BattleManager/PhaseState/PreparePhase.cs
+++ Assets/Script/Manager/BattleManager/PhaseState/PreparePhase.cs
@@ -7,7 +7,7 @@ public class PreparePhase : Phase
 
     public override void OnStateUpdate()
     {
-        _controller.ChangePhase(_controller.Engage);
+        
     }
 
     public override void OnStateExit()
