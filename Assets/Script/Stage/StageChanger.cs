81e1e6d977a93953b19d8eda1f05b324657a5f8a
dylk92 <dylk92@naver.com>
2023-05-06 15:12:40 +0900

    fix

--- Assets/Script/Stage/StageChanger.cs
+++ Assets/Script/Stage/StageChanger.cs
@@ -46,12 +46,13 @@ public class StageChanger
     {
         SetSpawnUnit(stage.BattleStageData.faction, stage.BattleStageData.level, stage.BattleStageData.id);
 
-        SceneChanger.SceneChange("JS TEST");
-        //SceneChanger.SceneChange("Battle");
+        //SceneChanger.SceneChange("JS TEST");
+        SceneChanger.SceneChange("Battle");
     }
 
     private void SetSpawnUnit(Faction faction, int level, int id) // 다음에 받을 팩션, 레벨, 아이디 넣기
     {
+        Debug.Log(faction + ", " + level + ", " + id);
         GameManager.Data.CurrentStageData = GameManager.Data.StageDatas.GetStageData(faction.ToString(), level, id);
     }
 }
