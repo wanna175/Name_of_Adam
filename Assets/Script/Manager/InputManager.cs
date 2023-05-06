12a1a6d868a6c2d15d2315441a7431ec4b91fd73
Yeogiwon <84721859+Yeogiwon@users.noreply.github.com>
2023-05-06 18:58:11 +0900

    이미지 추가, inputManager 추가
    이미지 추가, inputManager 추가

---
+++ Assets/Script/Manager/InputManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameManager.Sound.Play("UIClick2");
+            Debug.Log("마우스 좌클릭");
+        }
+    }
+}
