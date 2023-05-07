6458805a10e0ac680ef553aa91da9984c20e0121
Yeogiwon <84721859+Yeogiwon@users.noreply.github.com>
2023-05-07 16:30:59 +0900

    UI 일부 수정, main Scene 이미지 적용, turn count 제거
    UI 일부 수정, main Scene 이미지 적용, turn count 제거

--- Assets/Script/Manager/InputManager.cs
+++ Assets/Script/Manager/InputManager.cs
@@ -11,10 +11,6 @@ public class InputManager : MonoBehaviour
             GameManager.Sound.Play("UIClick1");
             Debug.Log("마우스 좌클릭");
         }
-        else if(Input.GetMouseButtonDown(1))
-        {
-            GameManager.Sound.Play("BattleBGMA", Sounds.BGM);
-            Debug.Log("마우스 우클릭");
-        }
+        
     }
 }
