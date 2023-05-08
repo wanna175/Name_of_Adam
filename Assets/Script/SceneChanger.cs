e7fbbdd625c8d7a5c1fad1b71fe497aaa1989375
여기원 <ygw8253@gmail.com>
2023-05-09 03:17:43 +0900

    StageEnterResource 추가, 컷씬 브금 추가
    StageEnterResource 추가, 컷씬 브금 추가, 컷씬 순서 변경

--- Assets/Script/SceneChanger.cs
+++ Assets/Script/SceneChanger.cs
@@ -8,6 +8,12 @@ public static class SceneChanger
     
     public static void SceneChange(string SceneName)
     {
+        if(SceneName == "Battle")
+        {
+            GameManager.Sound.Clear();
+            GameManager.Sound.Play("Stage_Transition/Stage_Enter/Stage_EnterSFX");
+        }
+
         if(SceneName != "LogoScene")
         {
             GameManager.Sound.Clear();
