dbe8d1f3e0a6ce96d625156312654b814b1496ba
ch-0012 <73748422+ch-0012@users.noreply.github.com>
2023-05-17 21:12:47 +0900

    feature : Passive Manager 추가

--- Assets/Script/Unit/Passvie/Passive.cs
+++ Assets/Script/Unit/Passvie/Passive.cs
@@ -29,6 +29,9 @@ public class Passive : MonoBehaviour
     [SerializeField] private bool _isSpecial = false;
     public bool IsSpecial => _isSpecial;
 
+    // Memo : 사실 윗 부분만 있다면 SO로 떼서 관리하는 편이 나음...
+    // Use의 내용이 달라서 Prefab으로 관리하는 건데 이를 개선할 방법은 없을까
+
     public virtual void Use(BattleUnit caster, BattleUnit receiver)
     {
         return;
