58292f9a8de6baaeec7b01f59d5fde9bb27d0b8b
aiken516 <51198185+aiken516@users.noreply.github.com>
2023-05-18 18:59:03 +0900

    refactorig : 턴 알림 리팩토링

--- Assets/Script/UI/Scene/UI_TurnNotify.cs
+++ Assets/Script/UI/Scene/UI_TurnNotify.cs
@@ -6,33 +6,30 @@ using UnityEngine.EventSystems;
 
 public class UI_TurnNotify : UI_Scene
 {
-    [SerializeField] public GameObject image;
-
     public void Set(string turn)
     {
         FadeIn();
         Invoke("FadeOut", 0.5f);
 
         if (turn == "playerTurn")
-            image.GetComponent<Image>().sprite = GameManager.Resource.Load<Sprite>($"Arts/UI/Battle_UI/Text/PlayerTurnText");
+            GetComponent<Image>().sprite = GameManager.Resource.Load<Sprite>($"Arts/UI/Battle_UI/Text/PlayerTurnText");
         else if (turn == "UnitTurn")
-            image.GetComponent<Image>().sprite = GameManager.Resource.Load<Sprite>($"Arts/UI/Battle_UI/Text/UnitTurnText");
+            GetComponent<Image>().sprite = GameManager.Resource.Load<Sprite>($"Arts/UI/Battle_UI/Text/UnitTurnText");
     }
 
     public void FadeIn()
     {
-        image.GetComponent<FadeController>().StartFadeIn();
+        GetComponent<FadeController>().StartFadeIn();
     }
 
     public void FadeOut()
     {
-        image.GetComponent<FadeController>().StartFadeOut();
+        GetComponent<FadeController>().StartFadeOut();
         Invoke("Destroy", 0.5f);
     }
 
     private void Destroy()
     {
-        Debug.Log("DESTROY");
         GameManager.Resource.Destroy(this.gameObject);
     }
 }
