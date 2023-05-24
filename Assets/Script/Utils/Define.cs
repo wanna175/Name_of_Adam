22b78928e8e139bb857302342fcdd9ef09aca8da
ch-0012 <73748422+ch-0012@users.noreply.github.com>
2023-05-24 20:21:31 +0900

    feature : Enum 로마 숫자 추가

--- Assets/Script/Utils/Define.cs
+++ Assets/Script/Utils/Define.cs
@@ -128,3 +128,16 @@ public enum PassiveType
     ETC 
 };
 
+public enum RomanNumber
+{
+    I = 1,
+    II = 2,
+    III = 3,
+    IV = 4,
+    V = 5,
+    VI = 6,
+    VII = 7,
+    VIII = 8,
+    IX = 9,
+    X = 10,
+}
