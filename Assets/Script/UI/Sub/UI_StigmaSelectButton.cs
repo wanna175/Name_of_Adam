6c896d635e3ef4baa69553008ef4f75c379df7a0
aiken516 <51198185+aiken516@users.noreply.github.com>
2023-06-24 16:10:10 +0900

    refactoring : 낙인 선택지를 범용적이게 수정

---
+++ Assets/Script/UI/Sub/UI_StigmaSelectButton.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class UI_StigmaSelectButton : UI_Base
+{
+    [SerializeReference] private Image _stigmaImage;
+    [SerializeReference] private TextMeshProUGUI _stigmaName;
+    [SerializeReference] private TextMeshProUGUI _stigmaDescription;
+
+    private UI_StigmaSelectButtonPopup _popup;
+    private Passive _stigma;
+
+    public void init(UI_StigmaSelectButtonPopup popup, Passive stigma)
+    {
+        _popup = popup;
+        _stigma = stigma;
+
+        _stigmaName.text = stigma.GetName(false);
+        _stigmaDescription.text = stigma.Description;
+        _stigmaImage.sprite = stigma.Sprite;
+    }
+
+    public void OnClick()
+    {
+        _popup.OnClick(_stigma);
+    }
+}
