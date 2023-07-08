6ccc5d071f303ba5334e3ca18d760f44e5f6915d
Yeogiwon <84721859+Yeogiwon@users.noreply.github.com>
2023-07-08 17:27:32 +0900

    프리팹의 baseunit 추가

--- Assets/Script/Scenes/EventScene/HarlotScene/HarlotSceneController.cs
+++ Assets/Script/Scenes/EventScene/HarlotScene/HarlotSceneController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class WhoreSceneController : MonoBehaviour
+public class HarlotSceneController : MonoBehaviour
 {
     private DeckUnit _stigmatizeUnit;
 
