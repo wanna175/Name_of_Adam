de6ab2891f709b0ad59debb0368cb22ccce59619
Yeogiwon <84721859+Yeogiwon@users.noreply.github.com>
2023-07-19 19:10:20 +0900

    EventScene 기능 구현
    낙인소와 강화소의 모든 기능을 구현하였습니다. 다만 낙인소는 나갈 때의 코드를 조금 손 봐야하고 탕녀는 타락 관련 유닛과 낙인이 적어서 임시로 랜덤으로 적용했습니다. 추후 낙인 생성도 parameter에 따라 생성되는 낙인이 달라지도록 조정하겠습니다.

--- Assets/Script/Scenes/EventScene/UpgradeScene/UpgradeSceneController.cs
+++ Assets/Script/Scenes/EventScene/UpgradeScene/UpgradeSceneController.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class UpgradeSceneController : MonoBehaviour
 {
-    private DeckUnit _upgradeUnit;
+    private DeckUnit _unit;
 
     [SerializeField] private Image _upgradeunitImage; // 강화 대상 유닛
 
@@ -28,15 +28,41 @@ public class UpgradeSceneController : MonoBehaviour
         GameManager.UI.ShowPopup<UI_Conversation>().Init(scripts);
     }
 
+    // 업그레이드 할 유닛을 고릅니다.
     public void OnUpgradeUnitButtonClick()
     {
-        GameManager.UI.ShowPopup<UI_MyDeck>("UI_MyDeck").Init(false, OnSelect);
+        GameManager.UI.ShowPopup<UI_MyDeck>("UI_MyDeck").Init(false, OnSelectUpgrade);
+    }
+
+    // 교화를 풀 유닛을 고릅니다.
+    public void OnReleaseUnitButtonClick()
+    {
+        GameManager.UI.ShowPopup<UI_MyDeck>("UI_MyDeck").Init(false, OnSelectRelease);
+    }
+
+    // 유닛 선택 후 업그레이드 완료 버튼
+    public void OnUpgradeButtonClick()
+    {
+        if (_unit != null)
+            GameManager.UI.ShowPopup<UI_UpgradeSelectButton>().init(this);
         //GameManager.Sound.Play("UI/ButtonSFX/ButtonClickSFX");
     }
 
-    public  void OnSelect(DeckUnit unit)
+    // 유닛 선택 후 교화 풀기 버튼
+    public void OnReleaseButtonClick()
     {
-        _upgradeUnit = unit;
+        if(_unit.DeckUnitChangedStat.FallCurrentCount > 0)
+        {
+            _unit.DeckUnitChangedStat.FallCurrentCount -= 1;
+        }
+
+        StartCoroutine(QuitScene());
+
+    }
+
+    public  void OnSelectUpgrade(DeckUnit unit)
+    {
+        _unit = unit;
         _upgradeunitImage.sprite = unit.Data.Image;
         _upgradeunitImage.color = Color.white;
 
@@ -44,12 +70,18 @@ public class UpgradeSceneController : MonoBehaviour
         GameManager.UI.ClosePopup();
     }
 
-    public void OnUpgradeButtonClick()
+    public void OnSelectRelease(DeckUnit unit)
     {
-        if (_upgradeUnit != null)
-            GameManager.UI.ShowPopup<UI_UpgradeSelectButton>().init(this);
-        GameManager.Sound.Play("UI/ButtonSFX/ButtonClickSFX");
+        _unit = unit;
+        _releaseunitImage.sprite = unit.Data.Image;
+        _releaseunitImage.color = Color.white;
+
+        GameManager.UI.ClosePopup();
+        GameManager.UI.ClosePopup();
     }
+
+
+    
 
     public void OnUpgradeSelect(int select) 
     {
@@ -58,22 +90,22 @@ public class UpgradeSceneController : MonoBehaviour
 
         if (select == 1)
         {
-            _upgradeUnit.DeckUnitUpgradeStat.ATK += 5;
+            _unit.DeckUnitUpgradeStat.ATK += 5;
             script.Init(GameManager.Data.ScriptData["강화소_공격력"], false);
         }
         else if (select == 2) 
         {
-            _upgradeUnit.DeckUnitUpgradeStat.MaxHP += 15;
+            _unit.DeckUnitUpgradeStat.MaxHP += 15;
             script.Init(GameManager.Data.ScriptData["강화소_체력"], false);
         }
         else if (select == 3)
         {
-            _upgradeUnit.DeckUnitUpgradeStat.SPD += 25;
+            _unit.DeckUnitUpgradeStat.SPD += 25;
             script.Init(GameManager.Data.ScriptData["강화소_속도"], false);
         }
         else if (select == 4)
         {
-            _upgradeUnit.DeckUnitUpgradeStat.ManaCost -= 10;
+            _unit.DeckUnitUpgradeStat.ManaCost -= 10;
             script.Init(GameManager.Data.ScriptData["강화소_코스트"], false);
         }
         GameManager.Sound.Play("UI/UpgradeSFX/UpgradeSFX");
@@ -89,7 +121,6 @@ public class UpgradeSceneController : MonoBehaviour
         {
             GameManager.Data.GameData.isVisitUpgrade = true;
         }
-        GameManager.Sound.Play("UI/ButtonSFX/ButtonClickSFX");
     }
 
     private IEnumerator QuitScene(UI_Conversation eventScript = null)
