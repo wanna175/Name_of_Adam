d8dffb80faf40498af93bacccc7287e31cfdc157
Yeogiwon <84721859+Yeogiwon@users.noreply.github.com>
2023-08-05 19:15:37 +0900

    새로운 Battle Scene UI 추가 및 프리팹 생성
    UI 측에서 수정해준 전투 씬 UI를 Battle Test UI에 적용시켰습니다.

--- Assets/Script/UI/Popup/UI_Info.cs
+++ Assets/Script/UI/Popup/UI_Info.cs
@@ -86,11 +86,11 @@ public class UI_Info : UI_Scene
         _hpBar.RefreshHPBar((float)battleUnit.BattleUnitTotalStat.CurrentHP / (float)battleUnit.BattleUnitTotalStat.MaxHP);
         _hpBar.RefreshFallGauge(battleUnit.BattleUnitTotalStat.FallCurrentCount);
 
-        unit.SetStigma();
-        foreach (Stigma sti in unit.Stigma)
-        {
-            GameObject.Instantiate(_stigma, _stigmaGrid).GetComponent<UI_HoverImageBlock>().Set(sti.Sprite, sti.Description);
-        }
+        //unit.SetStigma();
+        //foreach (Stigma sti in unit.Stigma)
+        //{
+        //    GameObject.Instantiate(_stigma, _stigmaGrid).GetComponent<UI_HoverImageBlock>().Set(sti.Sprite, sti.Description);
+        //}
 
         Sprite attackType;
 
@@ -168,11 +168,11 @@ public class UI_Info : UI_Scene
         _hpBar.RefreshHPBar((float)unit.DeckUnitTotalStat.CurrentHP / (float)unit.DeckUnitTotalStat.MaxHP);
         _hpBar.RefreshFallGauge(unit.DeckUnitTotalStat.FallCurrentCount);
 
-        unit.SetStigma();
-        foreach (Stigma sti in unit.Stigma)
-        {
-            GameObject.Instantiate(_stigma, _stigmaGrid).GetComponent<UI_HoverImageBlock>().Set(sti.Sprite, sti.Description);
-        }
+        //unit.SetStigma();
+        //foreach (Stigma sti in unit.Stigma)
+        //{
+        //    GameObject.Instantiate(_stigma, _stigmaGrid).GetComponent<UI_HoverImageBlock>().Set(sti.Sprite, sti.Description);
+        //}
 
         Sprite attackType;
 
--- Assets/Script/UI/Scene/UI_ManaGauge.cs
+++ Assets/Script/UI/Scene/UI_ManaGauge.cs
@@ -6,20 +6,24 @@ using TMPro;
 
 public class UI_ManaGauge : UI_Scene
 {
-    enum Objects
-    {
-        Fill,
-        ManaCostText,
-    }
+    [SerializeField] Image _bluemanaIMG;
+    [SerializeField] TextMeshProUGUI _currentMana;
 
-    private void Awake()
-    {
-        Bind<GameObject>(typeof(Objects));
-    }
+
+    //enum Objects
+    //{
+    //    Fill,
+    //    ManaCostText,
+    //}
+
+    //private void Awake()
+    //{
+    //    Bind<GameObject>(typeof(Objects));
+    //}
     
     public void DrawGauge(int max, int current)
     {
-        GetObject((int)Objects.Fill).GetComponent<Image>().fillAmount = (float)current / max;
-        GetObject((int)Objects.ManaCostText).GetComponent<TextMeshProUGUI>().text = current.ToString();
+        _bluemanaIMG.fillAmount = (float)current / max;
+        _currentMana.text = current.ToString();
     }
 }
