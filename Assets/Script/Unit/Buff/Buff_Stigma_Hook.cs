dc5f96024cf8f68800f74018ba5ada42661f4c11
aiken516 <51198185+aiken516@users.noreply.github.com>
2023-08-08 00:23:09 +0900

    feature : 갈고리 낙인 추가

--- Assets/Script/Unit/Buff/Buff_Stigma_Hook.cs
+++ Assets/Script/Unit/Buff/Buff_Stigma_Hook.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Buff_Stigma_Hook : Buff
 {    public override void Init(BattleUnit caster, BattleUnit owner)
@@ -28,7 +29,41 @@ public class Buff_Stigma_Hook : Buff
 
     public override bool Active(BattleUnit caster, BattleUnit receiver)
     {
-        
+        List<Vector2> UDLR = new() { Vector2.right, Vector2.up, Vector2.left, Vector2.down };
+
+        Vector2 casterPosition = caster.Location;
+        Vector2 receiverPosition = receiver.Location;
+        float currntMin = 100f;
+
+        List<Vector2> moveVectorList = new();
+
+        foreach (Vector2 direction in UDLR)
+        {
+            Vector2 vec = new(receiverPosition.x + direction.x, receiverPosition.y + direction.y);
+            float sqr = (vec - casterPosition).sqrMagnitude;
+
+            if (!BattleManager.Field.IsInRange(vec))
+                continue;
+
+            if (currntMin > sqr)
+            {
+                currntMin = sqr;
+                moveVectorList.Clear();
+                if (!BattleManager.Field.TileDict[vec].UnitExist)
+                {
+                    moveVectorList.Add(vec);
+                }
+            }
+            else if (currntMin == sqr)
+            {
+                moveVectorList.Add(vec);
+            }
+        }
+
+        if (moveVectorList.Count == 0)
+            BattleManager.Field.MoveUnit(receiver.Location, receiverPosition);
+        else
+            BattleManager.Field.MoveUnit(receiver.Location, moveVectorList[Random.Range(0, moveVectorList.Count)]);
 
         return false;
     }
