08b287506da778ef1394efc6762b03889e809ef4
aiken516 <51198185+aiken516@users.noreply.github.com>
2023-08-09 14:24:45 +0900

    feature : 피의 축복 낙인 추가

---
+++ Assets/Script/Unit/Stigma/Stigma_BloodBlessing.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class Stigma_BloodBlessing : Stigma
+{
+    public override void Use(BattleUnit caster, BattleUnit receiver)
+    {
+        base.Use(caster, receiver);
+
+        Buff_Stigma_BloodBlessing bloodBlessing = new();
+        if (Tier == StigmaTier.Tier1)
+        {
+            bloodBlessing.SetValue(10);
+        }
+        else if (Tier == StigmaTier.Tier2)
+        {
+            bloodBlessing.SetValue(20);
+        }
+        caster.SetBuff(bloodBlessing, caster);
+    }
+}
