569cdf4bde51e5bdc101e9ec79283108663b3484
gus6615 <wns6615@gmail.com>
2023-12-27 21:22:36 +0900

    feat: 투명도에 따른 클릭 이벤트 관리 스크립트 추가

--- Assets/Script/UI/AlphaClicker.cs
+++ Assets/Script/UI/AlphaClicker.cs
@@ -5,12 +5,21 @@ using UnityEngine.UI;
 
 public class AlphaClicker : MonoBehaviour
 {
+    private const float DISABLE_THRESHOLD = 0f;
+    private const float ENABLE_THRESHOLD = 0.1f;
+
     [SerializeField]
-    private float alphaThreshold = 0f;
+    private bool isEnable;
 
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Image>().alphaHitTestMinimumThreshold = alphaThreshold;
+        SetEnable(isEnable);
+    }
+
+    public void SetEnable(bool isEnable)
+    {
+        GetComponent<Image>().alphaHitTestMinimumThreshold = (isEnable) ? ENABLE_THRESHOLD : DISABLE_THRESHOLD;
+        this.isEnable = isEnable;
     }
 }
