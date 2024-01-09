57536730deb212f61e1f97108d8bb20eb01e144c
KelcyR <kelcyryu@gmail.com>
2024-01-10 03:25:54 +0900

    feature : 화신 선택 씬 제작
    - 화신 선택 씬 제작

--- Assets/Script/Scenes/DifficultySelectScene/DifficultySelectSceneController.cs
+++ Assets/Script/Scenes/DifficultySelectScene/DifficultySelectSceneController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class DifficultySelectSceneController : MonoBehaviour
 {
     [SerializeField] GameObject UI_Difficulty;
-    [SerializeField] GameObject UI_ChampionSelect;
-    [SerializeField] GameObject UI_PlayerSkillSelect;
+    [SerializeField] GameObject UI_IncarnaSelect;
     [SerializeField] GameObject UI_HallSelect;
+    [SerializeField] GameObject UI_ConfirmBtn;
 
+    public GameObject UI_Incarna_List;
+    public List<GameObject> Incarna_Info;
 
+    private Incarna incarnaData;
     private bool DifficultySelected;
 
     void Start()
@@ -20,29 +23,65 @@ public class DifficultySelectSceneController : MonoBehaviour
     private void Init()
     {
         UI_Difficulty.SetActive(false);//추후 순서대로 수정
-        UI_ChampionSelect.SetActive(false);
-        UI_PlayerSkillSelect.SetActive(false);
-        UI_HallSelect.SetActive(true);
+        UI_IncarnaSelect.SetActive(true);
+        UI_HallSelect.SetActive(false);
+        UI_ConfirmBtn.SetActive(false);
         DifficultySelected = false;
-
     }
 
     public void Confirm()
     {
-        if (UI_HallSelect)
+        GameManager.Sound.Play("UI/ButtonSFX/UIButtonClickSFX");
+
+        if (UI_IncarnaSelect.activeSelf == true)
+        {
+            //화신 데이터에 저장
+            GameManager.Data.GameDataMain.Incarna = incarnaData;
+            UI_IncarnaSelect.SetActive(false);
+            UI_HallSelect.SetActive(true);
+        }
+        else if (UI_HallSelect.activeSelf == true)
         {
             GameManager.Data.MainDeckSet();
             GameManager.Data.GameData.FallenUnits.Clear();
             GameManager.Data.GameData.FallenUnits.AddRange(GameManager.Data.GameDataMain.DeckUnits);
-        }
 
-        GameManager.Sound.Play("UI/ButtonSFX/UIButtonClickSFX");
-        SceneChanger.SceneChange("StageSelectScene");
+            SceneChanger.SceneChange("StageSelectScene");
+        }
+    }
+
+    public void OnIncarnaClick(int i)
+    {
+        UI_Incarna_List.SetActive(false);
+        UI_ConfirmBtn.SetActive(true);
+        Incarna_Info[i].SetActive(true);
+        incarnaData = GameManager.Resource.Load<Incarna>($"ScriptableObject/Incarna/{Incarna_Info[i].name}");
     }
 
     public void Quit()
     {
         GameManager.Sound.Play("UI/ButtonSFX/BackButtonClickSFX");
-        SceneChanger.SceneChange("MainScene");
+
+        if (UI_IncarnaSelect.activeSelf == true)
+        {
+            if (UI_ConfirmBtn.activeSelf == false)
+            {
+                SceneChanger.SceneChange("MainScene");
+            }
+            else
+            {
+                Incarna_Info.ForEach(obj => { obj.SetActive(false); });
+                UI_ConfirmBtn.SetActive(false);
+                UI_Incarna_List.SetActive(true);
+            }
+        }
+        else if (UI_HallSelect.activeSelf == true)
+        {
+            UI_HallSelect.SetActive(false);
+            UI_IncarnaSelect.SetActive(true);
+            Incarna_Info.ForEach(obj => { obj.SetActive(false); });
+            UI_ConfirmBtn.SetActive(false);
+            UI_Incarna_List.SetActive(true);
+        }
     }
 }
