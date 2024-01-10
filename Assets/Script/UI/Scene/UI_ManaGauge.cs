290cc74b730e9c7ce3d77628c2f41f8fe3f1b0e3
KelcyR <kelcyryu@gmail.com>
2024-01-10 16:59:12 +0900

    feature : 마나 게이지 애니메이션 적용

--- Assets/Script/UI/Scene/UI_ManaGauge.cs
+++ Assets/Script/UI/Scene/UI_ManaGauge.cs
@@ -6,7 +6,7 @@ using TMPro;
 
 public class UI_ManaGauge : UI_Scene
 {
-    [SerializeField] Image _bluemanaIMG;
+    [SerializeField] Animator ManaAnimator;
     [SerializeField] TextMeshProUGUI _currentMana;
 
 
@@ -23,7 +23,56 @@ public class UI_ManaGauge : UI_Scene
     
     public void DrawGauge(int max, int current)
     {
-        _bluemanaIMG.fillAmount = (float)current / max;
+        //_bluemanaIMG.fillAmount = (float)current / max;
+        SetGauge(current);
         _currentMana.text = current.ToString();
+    }
+
+    public void SetGauge(int mana)
+    {
+        if(mana == 0)
+        {
+            ManaAnimator.SetTrigger("Mana0");
+        }
+        else if(0 < mana && mana < 15)
+        {
+            ManaAnimator.SetTrigger("Mana10");
+        }
+        else if (15 <= mana && mana < 25)
+        {
+            ManaAnimator.SetTrigger("Mana20");
+        }
+        else if (25 <= mana && mana < 35)
+        {
+            ManaAnimator.SetTrigger("Mana30");
+        }
+        else if (35 <= mana && mana < 45)
+        {
+            ManaAnimator.SetTrigger("Mana40");
+        }
+        else if (45 <= mana && mana < 55)
+        {
+            ManaAnimator.SetTrigger("Mana50");
+        }
+        else if (55 <= mana && mana < 65)
+        {
+            ManaAnimator.SetTrigger("Mana60");
+        }
+        else if (65 <= mana && mana < 75)
+        {
+            ManaAnimator.SetTrigger("Mana70");
+        }
+        else if (75 <= mana && mana < 85)
+        {
+            ManaAnimator.SetTrigger("Mana80");
+        }
+        else if (85 <= mana && mana < 100)
+        {
+            ManaAnimator.SetTrigger("Mana90");
+        }
+        else if (mana == 100)
+        {
+            ManaAnimator.SetTrigger("Mana100");
+        }
     }
 }
