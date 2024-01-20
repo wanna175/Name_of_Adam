f0c546f466853af747f2385999e3a54a0412b1c7
gus6615 <wns6615@gmail.com>
2024-01-20 19:13:54 +0900

    feat: 전당 유닛 선택 구현

--- Assets/Script/UI/Sub/UI_HallCard.cs
+++ Assets/Script/UI/Sub/UI_HallCard.cs
@@ -1,14 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Rendering;
 
-public class UI_HallCard : MonoBehaviour
+public class UI_HallCard : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] 
+    private GameObject _highlight;
+
+    [SerializeField]
+    private List<GameObject> _stigmaFrames;
+
+    private List<Image> _stigmaImages;
+
+    [SerializeField]
+    private TMP_Text _nameText;
+
+    [SerializeField]
+    private GameObject infoButton;
+
     public Image UnitImage;
     private List<DeckUnit> _mainDeck;
     private List<HallUnit> _hallUnitList;
 
+    private bool _isEnable;
     public bool _isElite;
     public int HallUnitID; //µ¦°ú ´ëÄª½ÃÅ³ ID
 
@@ -22,13 +42,50 @@ public class UI_HallCard : MonoBehaviour
         _hallUnitList = GameManager.OutGameData.FindHallUnitList();
         _mainDeck = GameManager.Data.GameDataMain.DeckUnits;
 
+        _stigmaImages = new List<Image>();
+        foreach (var frame in _stigmaFrames)
+            _stigmaImages.Add(frame.GetComponentsInChildren<Image>()[1]);
+
         if (_mainDeck.Count <= HallUnitID)
         {
+            _isEnable = false;
+            DisableUI();
             return;
         }
 
+        foreach (var frame in _stigmaFrames)
+            frame.SetActive(true);
+
+        _isEnable = true;
         UnitImage.sprite = _mainDeck[HallUnitID].Data.Image;
         UnitImage.color = Color.white;
+        _nameText.SetText(_mainDeck[HallUnitID].Data.name);
+        infoButton.SetActive(true);
+
+        List<Stigma> stigmas = _mainDeck[HallUnitID].GetStigma();
+        for (int i = 0; i < _stigmaImages.Count; i++)
+        {
+            if (i < stigmas.Count)
+            {
+                _stigmaImages[i].sprite = stigmas[i].Sprite_28;
+                _stigmaImages[i].color = Color.white;
+            }
+            else
+            {
+                _stigmaImages[i].color = new Color(1f, 1f, 1f, 0f);            
+            }
+        }
+
+        _highlight.SetActive(false);
+    }
+
+    private void DisableUI()
+    {
+        UnitImage.color = new Color(1f, 1f, 1f, 0f);
+        _nameText.SetText("");
+        infoButton.SetActive(false);
+        foreach (var frame in _stigmaFrames)
+            frame.SetActive(false);
     }
 
     public void OnClick()
@@ -61,5 +118,28 @@ public class UI_HallCard : MonoBehaviour
 
         GameManager.UI.ClosePopup();
         GameManager.UI.ClosePopup();
+
+        Init();
+    }
+
+    public void OnInfoButton()
+    {
+        GameManager.Sound.Play("UI/ButtonSFX/UIButtonClickSFX");
+        UI_UnitInfo ui = GameManager.UI.ShowPopup<UI_UnitInfo>("UI_UnitInfo");
+
+        ui.SetUnit(_mainDeck[HallUnitID]);
+        ui.Init();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (_isEnable)
+            _highlight.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (_isEnable)
+            _highlight.SetActive(false);
     }
 }
