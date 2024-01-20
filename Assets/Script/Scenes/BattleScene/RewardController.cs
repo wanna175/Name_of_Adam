a677571642af0a1fd177d2a54c3dfe7409ccbe23
wanna175 <jh5045@naver.com>
2024-01-20 20:02:14 +0900

    보상화면UI
    UI

--- Assets/Script/Scenes/BattleScene/RewardController.cs
+++ Assets/Script/Scenes/BattleScene/RewardController.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class RewardUnit
 {
     public string name { get; }
-    public int DarkEssence { get; }
+    public int DarkEssence { get; set; }
     public Sprite image { get; }
 
     public RewardUnit(string name, int DarkEssence,Sprite image)
@@ -27,40 +27,35 @@ public class RewardController
     {
         this.dic_units = new Dictionary<int, RewardUnit>();
         this.prev_DarkEssence = DarkEssence;
-        foreach (DeckUnit unit in units)
-        {
-            Debug.Log("배틀전아이디" + unit.UnitID + " 이름 " + unit.Data.Name);
-        }
-
 
         foreach (DeckUnit unit in units)
         {
             dic_units.Add(unit.UnitID, new RewardUnit(unit.Data.Name, unit.DeckUnitStat.FallCurrentCount, 
                 GameManager.Resource.Load<Sprite>($"Arts/Units/Unit_Portrait/" + unit.Data.Name + "_타락")));
-            Debug.Log("배틀전아이디"+unit.UnitID +" 이름 "+ unit.Data.Name);
         }
     }
-    public void RewardSetting(List<DeckUnit> units, UI_BattleOver rewardScene)//리워드씬에서 값 세팅해주기
+    public void RewardSetting(List<DeckUnit> units, UI_RewardScene rewardScene)//리워드씬에서 값 세팅해주기
     {
-        RewardUnit unit;
-        foreach (DeckUnit uni in units)
-        {
-            Debug.Log("배틀후아이디" + uni.UnitID + " 이름 " + uni.Data.Name);
-        }
-
+        Debug.Log("현재 덱에 있는 유닛 갯수 : " + units.Count);
+        rewardScene.Init(units.Count,GameManager.Data.DarkEssense - prev_DarkEssence);
+        RewardUnit unit,fallunit;
+        int Rewardidx = 0;
         for (int i=units.Count-1;i>=0;--i)
         {
             if(dic_units.TryGetValue(units[i].UnitID,out unit))//기존에 있던 친구들
             {
-                int reward = unit.DarkEssence - units[i].DeckUnitStat.FallCurrentCount;
-                Sprite sp = unit.image;
+                unit.DarkEssence -= units[i].DeckUnitStat.FallCurrentCount;
+                rewardScene.setContent(Rewardidx, unit);
                 dic_units.Remove(units[i].UnitID);
-                Debug.Log("기존 유닛정보 : " +unit.name+" 바뀐신앙값 : "+ reward);
             }
             else//새로 플레이어 덱에 들어온 친구들
             {
-                Debug.Log("welcome : " + units[i].Data.Name+" id : "+units[i].UnitID);
+                
+                fallunit = new RewardUnit(units[i].Data.Name, units[i].DeckUnitStat.FallCurrentCount, 
+                    GameManager.Resource.Load<Sprite>($"Arts/Units/Unit_Portrait/" + units[i].Data.Name + "_타락"));
+                rewardScene.setContent(Rewardidx, fallunit);
             }
+            Rewardidx++;
         }
         foreach(var u in dic_units)//죽은 친구들
         {
