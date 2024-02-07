ad6ffcff6de9ae66eee2475c7ea1e108912264aa
aiken516 <jimmy000516@naver.com>
2024-02-07 20:51:48 +0900

    feat : 전령 추가

--- Assets/Script/Unit/UnitAction/UnitAction_CenteredSplash.cs
+++ Assets/Script/Unit/UnitAction/UnitAction_CenteredSplash.cs
@@ -10,7 +10,7 @@ public class UnitAction_CenteredSplash : UnitAction
         if (hits.Count == 0)
             return false;
 
-        List<BattleUnit> units = BattleManager.Field.GetUnitsInRange(attackUnit.Location, attackUnit.GetSplashRange(attackUnit.Location, coord), attackUnit.Team == Team.Player ? Team.Enemy : Team.Player);
+        List<BattleUnit> units = BattleManager.Field.GetUnitsInRange(attackUnit.Location, attackUnit.GetSplashRange(coord, attackUnit.Location), attackUnit.Team == Team.Player ? Team.Enemy : Team.Player);
 
         BattleManager.Instance.AttackStart(attackUnit, units.Distinct().ToList());
         return true;
