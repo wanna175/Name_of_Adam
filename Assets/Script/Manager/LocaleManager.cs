16b6134886a0d611fd8be182155b5cb998cadcba
gus6615 <wns6615@gmail.com>
2024-02-24 20:29:54 +0900

    feat: 진척도 번역 추가

--- Assets/Script/Manager/LocaleManager.cs
+++ Assets/Script/Manager/LocaleManager.cs
@@ -52,6 +52,8 @@ public class LocaleManager : MonoBehaviour
         return "";
     }
 
+    public string GetLocalizedProgress(string progressInfo) => GetLocalizedString("ProgressSceneTable", progressInfo);
+
     public string GetLocalizedUpgrade(string upgradeInfo) => GetLocalizedString("UpgradeTable", upgradeInfo);
 
     public string GetLocalizedEventScene(string eventSceneStr) => GetLocalizedString("EventSceneTable", eventSceneStr);
