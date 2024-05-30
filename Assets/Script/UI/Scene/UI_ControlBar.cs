e6ffa571aea48d385da99e60d60656cf4b905712
gus6615 <wns6615@gmail.com>
2024-05-30 17:42:24 +0900

    feat: 유닛 턴 일 때 하단 바 암전

--- Assets/Script/UI/Scene/UI_ControlBar.cs
+++ Assets/Script/UI/Scene/UI_ControlBar.cs
@@ -11,26 +11,32 @@ public class UI_ControlBar : UI_Scene
     [SerializeField] public UI_DarkEssence UI_DarkEssence;
     [SerializeField] public UI_ManaGauge UI_ManaGauge;
     [SerializeField] public Animator UI_Aniamtor;
-    [SerializeField] private GameObject _active;
+    [SerializeField] private Image _active;
+    [SerializeField] private CanvasGroup canvasGroup;
+
+    private const float fadeSpeed = 1.0f;
+    private const float fadeMinAlpha = 0.5f;
 
     public void EndPlayerHit()
         => UI_Aniamtor.SetBool("isPlayerHit", false);
 
     public void ControlBarActive()
     {
-        _active.SetActive(true);
-        StartCoroutine(FadeIn());
+        _active.gameObject.SetActive(true);
+        StartCoroutine(FadeInActive());
+        StartCoroutine(FadeInCanvas());
+        UI_ManaGauge.SetColorManaText(new Color(0.4f, 0.4f, 0.4f));
     }
 
-    private IEnumerator FadeIn()
+    private IEnumerator FadeInActive()
     {
-        Color c = _active.GetComponent<Image>().color;
+        Color c = _active.color;
 
         while (c.a < 1)
         {
             c.a += 0.01f;
 
-            _active.GetComponent<Image>().color = c;
+            _active.color = c;
 
             yield return null;
         }
@@ -38,22 +44,48 @@ public class UI_ControlBar : UI_Scene
 
     public void ControlBarInactive()
     {
-        StartCoroutine(FadeOut());
+        StartCoroutine(FadeOutActive());
+        StartCoroutine(FadeOutCanvas());
+        UI_ManaGauge.SetColorManaText(Color.black);
     }
 
-    private IEnumerator FadeOut()
+    private IEnumerator FadeOutActive()
     {
-        Color c = _active.GetComponent<Image>().color;
+        Color c = _active.color;
 
         while (c.a > 0)
         {
             c.a -= 0.01f;
 
-            _active.GetComponent<Image>().color = c;
+            _active.color = c;
 
             yield return null;
         }
 
-        _active.SetActive(false);
+        _active.gameObject.SetActive(false);
+    }
+
+    private IEnumerator FadeInCanvas()
+    {
+        float alpha = canvasGroup.alpha;
+        while (alpha < 1.0f)
+        {
+            alpha += Time.deltaTime * fadeSpeed;
+            canvasGroup.alpha = alpha;
+            yield return null;
+        }
+        canvasGroup.alpha = 1.0f;
+    }
+
+    private IEnumerator FadeOutCanvas()
+    {
+        float alpha = canvasGroup.alpha;
+        while (alpha > fadeMinAlpha)
+        {
+            alpha -= Time.deltaTime * fadeSpeed;
+            canvasGroup.alpha = alpha;
+            yield return null;
+        }
+        canvasGroup.alpha = fadeMinAlpha;
     }
 }
