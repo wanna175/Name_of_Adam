c94aec912b47b8b1d73af00eee8a04c5e7f60f77
Cheonnyang <wns6615@gmail.com>
2024-06-22 00:51:09 +0900

    fix: 전당 삭제 및 덱빌딩 간의 버그 해결

--- Assets/Script/UI/Sub/UI_HallCard.cs
+++ Assets/Script/UI/Sub/UI_HallCard.cs
@@ -20,6 +20,7 @@ public class UI_HallCard : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     private List<Image> _stigmaImages;
     private List<DeckUnit> _mainDeck;
     private List<HallUnit> _hallUnitList;
+    private DeckUnit _deckUnit;
 
     private bool _isEnable;
     public bool _isElite;
@@ -31,31 +32,30 @@ public class UI_HallCard : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     {
         _hallUnitList = GameManager.OutGameData.FindHallUnitList();
         _mainDeck = GameManager.Data.GameDataMain.DeckUnits;
-
-        // _mainDeck을 HallUnitID 순서대로 정렬
         _mainDeck.Sort((x, y) => x.HallUnitID.CompareTo(y.HallUnitID));
 
-        _stigmaImages = new List<Image>();
-        foreach (var frame in _stigmaFrames)
-            _stigmaImages.Add(frame.GetComponentsInChildren<Image>()[1]);
-
-        if (_mainDeck.Count <= HallUnitID || _mainDeck[HallUnitID].IsMainDeck == false)
+        _deckUnit = _mainDeck.Find(x => x.HallUnitID == HallUnitID);
+        if (_deckUnit == null)
         {
             _isEnable = false;
             DisableUI();
             return;
         }
 
+        _stigmaImages = new List<Image>();
+        foreach (var frame in _stigmaFrames)
+            _stigmaImages.Add(frame.GetComponentsInChildren<Image>()[1]);
+
         foreach (var frame in _stigmaFrames)
             frame.SetActive(true);
 
         _isEnable = true;
-        UnitImage.sprite = _mainDeck[HallUnitID].Data.CorruptImage;
+        UnitImage.sprite = _deckUnit.Data.CorruptImage;
         UnitImage.color = Color.white;
-        _nameText.SetText(_mainDeck[HallUnitID].Data.Name);
+        _nameText.SetText(_deckUnit.Data.Name);
         infoButton.SetActive(true);
 
-        List<Stigma> stigmas = _mainDeck[HallUnitID].GetStigma();
+        List<Stigma> stigmas = _deckUnit.GetStigma();
         for (int i = 0; i < _stigmaImages.Count; i++)
         {
             if (i < stigmas.Count)
@@ -71,7 +71,7 @@ public class UI_HallCard : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
             }
         }
 
-        if (_mainDeck[HallUnitID].Data.Rarity == Rarity.Normal)
+        if (_deckUnit.Data.Rarity == Rarity.Normal)
         {
             _frameImage.sprite = NormalImage;
         }
@@ -131,10 +131,13 @@ public class UI_HallCard : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         DeckUnit afterDeckUnit = unit;
         HallUnit afterHallUnit = _hallUnitList.Find(x => x.ID == afterDeckUnit.HallUnitID);
 
-        if (_mainDeck.Count <= HallUnitID)
+        if (_mainDeck.Find(x => x.HallUnitID == HallUnitID) == null)
         {
             // 신규 유닛이면 추가
-            GameManager.Data.GameDataMain.DeckUnits.Add(unit);
+            if (_mainDeck.Count < HallUnitID)
+                _mainDeck.Add(unit);
+            else
+                _mainDeck.Insert(HallUnitID, unit);
         }
         else
         {
@@ -147,32 +150,14 @@ public class UI_HallCard : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
             beforeDeckUnit.HallUnitID = afterDeckUnit.HallUnitID;
             beforeHallUnit.ID = afterHallUnit.ID;
 
-            _mainDeck[HallUnitID].IsMainDeck = false;
-            _mainDeck[HallUnitID].HallUnitID = afterDeckUnit.HallUnitID;
+            _mainDeck[HallUnitID] = unit;
         }
-
-        // GameDataMain Deck 수정
-        _mainDeck[HallUnitID] = unit;
-        _mainDeck[HallUnitID].IsMainDeck = true;
-        _mainDeck[HallUnitID].HallUnitID = HallUnitID;
 
         // GameData Deck 수정
         afterDeckUnit.IsMainDeck = true;
         afterHallUnit.IsMainDeck = true;
         afterDeckUnit.HallUnitID = HallUnitID;
         afterHallUnit.ID = HallUnitID;
-
-        if (unit.Data.Rarity == Rarity.Normal)
-        {
-            _frameImage.sprite = NormalImage;
-        }
-        else
-        {
-            _frameImage.sprite = EliteImage;
-        }
-
-        UnitImage.sprite = _mainDeck[HallUnitID].Data.Image;
-        UnitImage.color = Color.white;
 
         GameManager.UI.ClosePopup();
         GameManager.UI.ClosePopup();
