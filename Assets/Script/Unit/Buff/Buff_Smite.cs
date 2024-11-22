cf5c817c5c997e273897e0092384a273a08c3183
aiken516 <jimmy000516@naver.com>
2024-11-23 01:49:27 +0900

    feature : 성흔 4개 추가

--- Assets/Script/Unit/Buff/Buff_Smite.cs
+++ Assets/Script/Unit/Buff/Buff_Smite.cs
@@ -1,19 +1,23 @@
 using UnityEngine;
-using System.Collections.Generic;
 
-public class Buff_Stigma_Hook : Buff
-{
-    public override void Init(BattleUnit owner)
+public class Buff_Smite : Buff
+{    public override void Init(BattleUnit owner)
     {
-        _buffEnum = BuffEnum.Stigmata_Hook;
+        _buffEnum = BuffEnum.Smite;
 
-        _name = "Hook";
+        _sprite = GameManager.Resource.Load<Sprite>($"Arts/Buff/Buff_Smite_Sprite");
+
+        _name = "Smite";
+
+        _description = "Smite Info";
+
+        _count = 1;
+
+        _countDownTiming = ActiveTiming.AFTER_ATTACK;
 
         _buffActiveTiming = ActiveTiming.AFTER_ATTACK;
 
         _owner = owner;
-
-        _stigmataBuff = true;
     }
 
     public override bool Active(BattleUnit caster)
@@ -24,13 +28,18 @@ public class Buff_Stigma_Hook : Buff
         if (caster.Data.ID == "¿æ_¸öÃ¼" || caster.Data.Rarity == Rarity.Boss || caster.DeckUnit.GetUnitSize() > 1)
             return false;
 
-        Vector2 hookDir = (_owner.Location - caster.Location).normalized;
+        Vector2 hookDir = (caster.Location - _owner.Location).normalized;
         Vector2Int hookDirInt = new Vector2Int(Mathf.RoundToInt(hookDir.x), Mathf.RoundToInt(hookDir.y));
         Vector2 vec = caster.Location + hookDirInt;
 
         if (BattleManager.Field.IsInRange(vec) && !BattleManager.Field.TileDict[vec].UnitExist)
-            BattleManager.Instance.MoveUnit(caster, vec);
+            BattleManager.Instance.MoveUnit(caster, vec, 2f, true);
 
         return false;
+    }
+
+    public override void Stack()
+    {
+        _count += 1;
     }
 }
